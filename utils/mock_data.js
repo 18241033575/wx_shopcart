85b8ae3e5fa2d11d52e2f4deda6c17d2fd25f6db
Jayshi <240638896@qq.com>
2019-08-22 15:50:58 +0800

    wx_shopcart

---
+++ utils/mock_data.js
@@ -0,0 +1,100 @@
+export const category_data = [
+  {
+    url: '/images/game.png',
+    name: '超市生鲜'
+  },
+  {
+    url: '/images/sugar.png',
+    name: '酒店住宿'
+  },
+  {
+    url: '/images/love.png',
+    name: '餐饮娱乐'
+  },
+  {
+    url: '/images/star.png',
+    name: '丽人美发'
+  },
+  {
+    url: '/images/gift.png',
+    name: '时尚购物'
+  },
+  {
+    url: '/images/coffee.png',
+    name: '家装家纺'
+  },
+  {
+    url: '/images/money.png',
+    name: '生活服务'
+  },
+  {
+    url: '/images/water.png',
+    name: '其他'
+  }
+]
+
+export const shop_category = [
+  {
+    id: 1,
+    name: '食品'
+  },
+  {
+    id: 2,
+    name: '母婴'
+  },
+  {
+    id: 3,
+    name: '淘淘'
+  },
+  {
+    id: 4,
+    name: '水果'
+  },
+  {
+    id: 5,
+    name: '女装'
+  },
+  {
+    id: 6,
+    name: '百货'
+  },
+  {
+    id: 7,
+    name: '美妆'
+  },
+  {
+    id: 8,
+    name: '电器'
+  },
+  {
+    id: 9,
+    name: '充值'
+  },
+  {
+    id: 10,
+    name: '男装'
+  }
+]
+
+export const shopping_category = [
+  {
+    id: 1,
+    name: '首页'
+  },
+  {
+    id: 2,
+    name: '生活家居'
+  },
+  {
+    id: 3,
+    name: '美妆洗护'
+  },
+  {
+    id: 4,
+    name: '食品生鲜'
+  },
+  {
+    id: 5,
+    name: '母婴用品'
+  }
+]
